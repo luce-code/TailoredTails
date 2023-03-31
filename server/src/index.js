9b4e1116436579a412abaa914ff39c478415b462
mohammadreza memar <moreza.memar@gmail.com>
2023-03-31 11:26:55 -0500

    create the api to check it for backend work

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,18 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import { userRouter } from "./routes/users.js";
+
 const app = express();
 
 // Middleware to parse JSON data
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
+
+mongoose.connect(
+  "mongodb+srv://TailoredTails:E3McVkTXMLTF0wUQ@tailoredtails.uarpfpi.mongodb.net/TailoredTails?retryWrites=true&w=majority"
+);
 
 app.listen(3003, () => console.log("SERVER STARTED"));
---
+++ server/src/models/Users.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /\S+@\S+\.\S+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const UserModel = mongoose.model("users", UserSchema);
