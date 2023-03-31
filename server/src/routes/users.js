625af0904da9c84b2cc770780beded756c3e6508
mohammadreza memar <moreza.memar@gmail.com>
2023-03-31 12:26:23 -0500

    working on login route

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -10,7 +10,25 @@ router.post("/register", async (req, res) => {
 
   const user = await UserModel.findOne({ username });
 
-  res.json(user);
+  if (user) {
+    return res.json({ message: "User Already Exists!" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.json({ message: "User Registered Successfully" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: " User Doesn't Exist!" });
+  }
 });
 
 export { router as userRouter };
