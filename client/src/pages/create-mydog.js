2b4f140124dde76e96cc7d999916272503c0245f
mohammadreza memar <moreza.memar@gmail.com>
2023-04-14 11:26:26 -0500

    check it now

--- client/src/pages/create-mydog.js
+++ client/src/pages/create-mydog.js
@@ -27,7 +27,7 @@ export const CreateMydog = () => {
     event.preventDefault();
     try {
       await axios.post(
-        "localhost:3001/mydogs/my-dog",
+        "http://localhost:3001/mydogs/my-dog",
         { ...mydogs },
         {
           headers: { Authorization: `Bearer ${cookies.access_token}` },
