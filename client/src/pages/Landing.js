8e91f6629b93110db96c973682934acfe0e7b70f
Andrew Cook <andrewjcook93@gmail.com>
2023-06-03 12:10:01 -0500

    styles: landing header

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -8,15 +8,16 @@ function Landing() {
         <div className="landing__header--left">
           <div className="landing__header--text-box">
             <span>The largest community of pet enthusiasts</span>
+            <br />
             <Link to="/login">Join Today</Link>
           </div>
         </div>
         <div className="landing__header--right">
-          
+
         </div>
       </div>
       <div className="landing__features-1">Features</div>
-      <div className="landing__feautres-2">Features 2</div>
+      <div className="landing__features-2">Features 2</div>
       <div className="landing__community">Community</div>
       <div className="landing__signup">SignUp</div>
 
