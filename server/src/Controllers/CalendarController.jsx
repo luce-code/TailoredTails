698ed4f39c207fcada0b04bb83bb41337f118aaa
mohammadreza memar <moreza.memar@gmail.com>
2023-06-08 10:06:53 -0500

    adding controllers on server

---
+++ server/src/Controllers/CalendarController.jsx
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const Event = require('../models/Event');
+const moment = require('moment');
+
+router.post({ path: '/create-event' }, async (req, res) => {
+  const event = Event(req.body);
+  await event.save();
+  res.sendStatus(201);
+});
+
+router.get({ path: '/get-events' }, async (req, res) => {
+  const events = await Event.find({
+    start: {
+      $gte: moment(req.query.start).toDate(),
+      $lt: moment(req.query.end).toDate(),
+    },
+  });
+
+  res.send(events);
+});
+
+module.exports = router;
