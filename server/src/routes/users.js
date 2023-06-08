125a96307c9080915ec6f60a01003dafdec48a18
mohammadreza memar <moreza.memar@gmail.com>
2023-06-08 10:10:17 -0500

    clean the code

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,17 +1,17 @@
-import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { UserModel } from "../models/Users.js";
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   const { username, password } = req.body;
 
   const user = await UserModel.findOne({ username });
 
   if (user) {
-    return res.json({ message: "User Already Exists!" });
+    return res.json({ message: 'User Already Exists!' });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -19,10 +19,10 @@ router.post("/register", async (req, res) => {
   const newUser = new UserModel({ username, password: hashedPassword });
   await newUser.save();
 
-  res.json({ message: "User Registered Successfully" });
+  res.json({ message: 'User Registered Successfully' });
 });
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await UserModel.findOne({ username });
 
@@ -33,10 +33,10 @@ router.post("/login", async (req, res) => {
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (!isPasswordValid) {
-    return res.json({ message: "Username or Password Is Incorrect! " });
+    return res.json({ message: 'Username or Password Is Incorrect! ' });
   }
 
-  const token = jwt.sign({ id: user._id }, "secret");
+  const token = jwt.sign({ id: user._id }, 'secret');
   res.json({ token, userID: user._id });
 });
 
