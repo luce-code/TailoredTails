7f707b44e015d7fdce7a2458d49890aed312499a
mohammadreza memar <moreza.memar@gmail.com>
2023-06-08 10:01:46 -0500

    set up the invironment for calendarin app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,61 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
-import { Auth } from "./pages/auth";
-import { CreateMydog } from "./pages/create-mydog";
-import { MyDog } from "./pages/my-dog";
-import Footer from "./components/Footer";
-import { Navbar } from "./components/navbar";
-import jwt_decode from "jwt-decode";
+import './App.css';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/home';
+import { Auth } from './pages/auth';
+import Modal from 'react-modal';
+import Calendar from './pages/Calendar';
+
+// import { CreateMydog } from "./pages/create-mydog";
+import { MyDog } from './pages/my-dog';
+import Footer from './components/Footer';
+import { Navbar } from './components/navbar';
+import jwt_decode from 'jwt-decode';
 // import RegisterPage from "./pages/register";
 
-import "./css/base/base.css"
-import "./css/base/typography.css"
-import "./css/base/variables.css"
-import "./css/components/petprofile.css"
-import "./css/layout/footer.css"
-import "./css/layout/navbar.css"
-import "./css/pages/kennel.css"
-import "./css/pages/landing.css"
+import './css/base/base.css';
+import './css/base/typography.css';
+import './css/base/variables.css';
+import './css/components/details.css';
+import './css/components/petprofile.css';
+import './css/layout/footer.css';
+import './css/layout/navbar.css';
+import './css/pages/kennel.css';
+import './css/pages/landing.css';
+
+Modal.setAppElement('#root');
 
 function App() {
   const [user, setUser] = useState({});
   function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
+    console.log('Encoded JWT ID token: ' + response.credential);
     var userObject = jwt_decode(response.credential);
     console.log(userObject);
     setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
+    document.getElementById('signInDiv').hidden = true;
   }
 
   function handlesignOut(event) {
     setUser({});
-    document.getElementById("signInDiv").hidden = false;
+    document.getElementById('signInDiv').hidden = false;
   }
   useEffect(() => {
     /*global google */
     google.accounts.id.initialize({
       client_id:
-        "556953690836-kvr32pa382819a251f3dmv4fiv3hbta3.apps.googleusercontent.com",
+        '556953690836-kvr32pa382819a251f3dmv4fiv3hbta3.apps.googleusercontent.com',
       callback: handleCallbackResponse,
     });
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outline",
-      size: "large",
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
     });
     google.accounts.id.prompt();
   }, []);
 
   return (
-    <div className="App">
-      <div id="signInDiv"></div>
+    <div className='App'>
+      <div id='signInDiv'></div>
       {Object.keys(user).length !== 0 && (
         <button onClick={(e) => handlesignOut(e)}>Sign Out</button>
       )}
@@ -62,11 +68,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Auth />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Auth />} />
+          <Route path='/Calendar' element={<Calendar />} />
           {/* <Route path="/register" element={<RegisterPage />} /> */}
-          <Route path="/create-mydog" element={<CreateMydog />} />
-          <Route path="/my-dog" element={<MyDog />} />
+          {/* <Route path="/create-mydog" element={<CreateMydog />} /> */}
+          <Route path='/my-dog' element={<MyDog />} />
         </Routes>
         <Footer />
       </Router>
