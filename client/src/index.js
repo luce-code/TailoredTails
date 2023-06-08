f6339301760f27fecc1d0cf5a685e0eeab6d65fa
mohammadreza memar <moreza.memar@gmail.com>
2023-06-08 10:03:39 -0500

    import React-datetime

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'react-datetime/css/react-datetime.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
