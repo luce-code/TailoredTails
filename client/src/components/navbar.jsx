5db43c6cd8f1902d93ceb609fdd9852b098d6bca
mohammadreza memar <moreza.memar@gmail.com>
2023-06-14 14:59:14 -0500

    fixed the logout error

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,15 +1,14 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import Calendar from "../pages/Calendar";
-
 
 export const Navbar = () => {
+  const navigate = useNavigate();
   const [cookies, removeCookies] = useCookies(["access_token"]);
-  // removed setCookies for now
 
   const handleLogout = () => {
     removeCookies("access_token");
-    console.log("Logout clicked"); // add this line
+    navigate("/login"); // Redirect to the login page after logout
   };
 
   return (
@@ -17,30 +16,65 @@ export const Navbar = () => {
       {!cookies.access_token ? (
         <div className="navbar__main">
           <div className="navbar-left">
-            <Link className="navbar__home" to="/"><img className="navbar__logo" src="https://i.imgur.com/c6GK0kY.png" alt="tailored tails logo" /></Link>
-            <Link className="navbar__button" to="/">Features</Link>
-            <Link className="navbar__button" to="/">Pricing</Link>
-            <Link className="navbar__button" to="/">Community</Link>
-            <Link className="navbar__button" to="/">Support</Link>
+            <Link className="navbar__home" to="/">
+              <img
+                className="navbar__logo"
+                src="https://i.imgur.com/c6GK0kY.png"
+                alt="tailored tails logo"
+              />
+            </Link>
+            <Link className="navbar__button" to="/">
+              Features
+            </Link>
+            <Link className="navbar__button" to="/">
+              Pricing
+            </Link>
+            <Link className="navbar__button" to="/">
+              Community
+            </Link>
+            <Link className="navbar__button" to="/">
+              Support
+            </Link>
           </div>
           <div className="navbar-right">
-            <Link className="navbar__button" to="/login">Login</Link>
-            <Link className="navbar__button" to="/login">Register</Link>
+            <Link className="navbar__button" to="/login">
+              Login
+            </Link>
+            <Link className="navbar__button" to="/login">
+              Register
+            </Link>
           </div>
         </div>
-
       ) : (
         <div className="navbar__main">
           <div className="navbar-left">
-          <Link className="navbar__home" to="/"><img className="navbar__logo" src="https://i.imgur.com/c6GK0kY.png" alt="tailored tails logo" /></Link>
-            <Link className="navbar__button" to="/">Kennel</Link>
-            <Link className="navbar__button" to="/">Routine</Link>
-            <Link className="navbar__button" to="/Calendar">Calendar</Link>
-            <Link className="navbar__button" to="/">Support</Link>
+            <Link className="navbar__home" to="/">
+              <img
+                className="navbar__logo"
+                src="https://i.imgur.com/c6GK0kY.png"
+                alt="tailored tails logo"
+              />
+            </Link>
+            <Link className="navbar__button" to="/">
+              Kennel
+            </Link>
+            <Link className="navbar__button" to="/">
+              Routine
+            </Link>
+            <Link className="navbar__button" to="/Calendar">
+              Calendar
+            </Link>
+            <Link className="navbar__button" to="/">
+              Support
+            </Link>
           </div>
           <div className="navbar-right">
-            <Link className="navbar__button" to="/login">Profile</Link>
-            <Link to="/" onClick={handleLogout} className="navbar__button">Logout</Link>
+            <Link className="navbar__button" to="/login">
+              Profile
+            </Link>
+            <button onClick={handleLogout} className="navbar__button">
+              Logout
+            </button>
           </div>
         </div>
       )}
